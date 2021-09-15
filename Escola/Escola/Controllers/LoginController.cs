508dc2f716048a248c0be5097353aeb78932fdf0
Fernando-Santana <Fernando-Santana@FERNANDO>
2021-09-15 20:22:05 -0300

    Inicio do projeto - Login responsivo com boostrap, com consulta no banco

---
+++ Escola/Escola/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Escola.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Json(new { Msg = "Usuário já logado" });
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Logar(string nome, string senha, bool manterLogado)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=EscolaDB;");
+            await sqlConnection.OpenAsync();
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = $"SELECT * FROM usuarios WHERE nome = '{nome}' AND senha = '{senha}'";
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (await reader.ReadAsync())
+            {
+
+                int usuarioId = reader.GetInt32(0);
+
+                List<Claim> direitosAcesso = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioId.ToString())
+                };
+
+                var identity = new ClaimsIdentity(direitosAcesso, "Identity.Login");
+                var userPrincipal = new ClaimsPrincipal(new[] { identity });
+
+                await HttpContext.SignInAsync(userPrincipal,
+                        new AuthenticationProperties
+                        {
+                            IsPersistent = manterLogado,
+                            ExpiresUtc = DateTime.Now.AddHours(1)
+                        }                    
+                    );
+
+                return Json(new { Msg = "Usuário logado com sucesso" });
+            }
+
+            return Json(new { Msg = "Usuário não encontrado" });
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
