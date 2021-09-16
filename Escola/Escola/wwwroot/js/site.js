3a552050da6787365469e3ee41029a4ab7acc436
Fernando-Santana <Fernando-Santana@FERNANDO>
2021-09-16 13:34:00 -0300

    Adicionando Jquery Ajax Unobtrusive - informações do CRUD

--- Escola/Escola/wwwroot/js/site.js
+++ Escola/Escola/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function Sucesso(data) {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: data.msg,
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
 
-// Write your JavaScript code.
+function Falha(data) {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Falhou',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
